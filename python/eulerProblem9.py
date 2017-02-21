e696e75db3ba656083b2e9703c2d4abe46b018f1
Sam Creamer <s.creamer@hotmail.com>
2017-02-20 19:14:25 -0500

    Added problem 9

---
+++ python/eulerProblem9.py
@@ -0,0 +1,9 @@
+# euler 9
+# sam creamer
+
+for a in range(1000):
+	for b in range(a + 1, 1000):
+		for c in range (b + 1, 1000):
+			if (a + b + c == 1000):
+				if (a**2 + b**2 == c**2):
+					print a*b*c
