76dff05702c87c513229fcf42d7b42d9598da5d6
Sam Creamer <creamers.mtl@gmail.com>
2019-12-11 10:39:21 -0500

    19

---
+++ python3/problem19.py
@@ -0,0 +1,11 @@
+import datetime
+
+counter = 0
+
+for i in range(1901, 2001):
+    for j in range(1, 13):
+        d = datetime.datetime(i, j, 1)
+        if d.weekday() == 6:
+            counter += 1
+
+print(counter)
