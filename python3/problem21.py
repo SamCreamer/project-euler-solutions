1ac009958a7689f5c551332cdb01a36d4368d16e
Sam Creamer <creamers.mtl@gmail.com>
2019-12-12 12:32:09 -0500

    problem21

---
+++ python3/problem21.py
@@ -0,0 +1,23 @@
+import math
+
+def sum_of_divisors(n):
+    divs = []
+    for i in range(1, int(n / 2) + 1):
+        if n % i == 0:
+            divs.append(i)
+    return sum(divs)
+
+checked = []
+result = 0
+
+for i in range(1, 10001):
+    if i in checked:
+        continue
+    fs = sum_of_divisors(i)
+    if i == sum_of_divisors(fs):
+        result += i
+        result += fs
+    checked.append(fs)
+    checked.append(i)
+
+print(result)
