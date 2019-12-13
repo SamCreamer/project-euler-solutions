7ac4978afbdf30aee1cc614df897321640b14272
Sam Creamer <creamers.mtl@gmail.com>
2019-12-13 13:06:16 -0500

    Abundant

--- python3/problem23.py
+++ python3/problem23.py
@@ -1,6 +1,16 @@
-def divisors(n):
+def proper_divisors(n):
     divs = []
     for i in range(1, int(n / 2) + 1):
         if n % i == 0:
             divs.append(i)
     return divs
+
+
+def abundant(n):
+    """
+    Returns True if the number is abundant. The definition of abundant is:
+    A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+    :param n:
+    :return:
+    """
+    return sum(proper_divisors(n)) > n
